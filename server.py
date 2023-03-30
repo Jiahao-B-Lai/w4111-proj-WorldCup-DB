4c5cdbd0a98feeb771832ac1eadb69cb11060740
jiahao_lai <jl6274@columbia.edu>
2023-03-30 20:14:07 +0000

    Show Group Scoreboards, next step is to show the match data and players data. Inserting data is still not available

--- server.py
+++ server.py
@@ -46,45 +46,45 @@ engine = create_engine(DATABASEURI)
 # Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
 #
 with engine.connect() as conn:
-	create_table_command = """
-	CREATE TABLE IF NOT EXISTS test (
-		id serial,
-		name text
-	)
-	"""
-	res = conn.execute(text(create_table_command))
-	insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
-	res = conn.execute(text(insert_table_command))
-	# you need to commit for create, insert, update queries to reflect
-	conn.commit()
+        create_table_command = """
+        CREATE TABLE IF NOT EXISTS test (
+                id serial,
+                name text
+        )
+        """
+        res = conn.execute(text(create_table_command))
+        insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
+        res = conn.execute(text(insert_table_command))
+        # you need to commit for create, insert, update queries to reflect
+        conn.commit()
 
 
 @app.before_request
 def before_request():
-	"""
-	This function is run at the beginning of every web request
-	(every time you enter an address in the web browser).
-	We use it to setup a database connection that can be used throughout the request.
+        """
+        This function is run at the beginning of every web request
+        (every time you enter an address in the web browser).
+        We use it to setup a database connection that can be used throughout the request.
 
-	The variable g is globally accessible.
-	"""
-	try:
-		g.conn = engine.connect()
-	except:
-		print("uh oh, problem connecting to database")
-		import traceback; traceback.print_exc()
-		g.conn = None
+        The variable g is globally accessible.
+        """
+        try:
+                g.conn = engine.connect()
+        except:
+                print("uh oh, problem connecting to database")
+                import traceback; traceback.print_exc()
+                g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-	"""
-	At the end of the web request, this makes sure to close the database connection.
-	If you don't, the database could run out of memory!
-	"""
-	try:
-		g.conn.close()
-	except Exception as e:
-		pass
+        """
+        At the end of the web request, this makes sure to close the database connection.
+        If you don't, the database could run out of memory!
+        """
+        try:
+                g.conn.close()
+        except Exception as e:
+                pass
 
 
 #
@@ -102,64 +102,99 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
-	"""
-	request is a special object that Flask provides to access web request information:
+        """
+        request is a special object that Flask provides to access web request information:
 
-	request.method:   "GET" or "POST"
-	request.form:     if the browser submitted a form, this contains the data in the form
-	request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+        request.method:   "GET" or "POST"
+        request.form:     if the browser submitted a form, this contains the data in the form
+        request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-	See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
-	"""
+        See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
+        """
 
-	# DEBUG: this is debugging code to see what request looks like
-	print(request.args)
-
-
-	#
-	# example of a database query
-	#
-	select_query = "SELECT name from test"
-	cursor = g.conn.execute(text(select_query))
-	names = []
-	for result in cursor:
-		names.append(result[0])
-	cursor.close()
-
-	#
-	# Flask uses Jinja templates, which is an extension to HTML where you can
-	# pass data to a template and dynamically generate HTML based on the data
-	# (you can think of it as simple PHP)
-	# documentation: https://realpython.com/primer-on-jinja-templating/
-	#
-	# You can see an example template in templates/index.html
-	#
-	# context are the variables that are passed to the template.
-	# for example, "data" key in the context variable defined below will be
-	# accessible as a variable in index.html:
-	#
-	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-	#     <div>{{data}}</div>
-	#
-	#     # creates a <div> tag for each element in data
-	#     # will print:
-	#     #
-	#     #   <div>grace hopper</div>
-	#     #   <div>alan turing</div>
-	#     #   <div>ada lovelace</div>
-	#     #
-	#     {% for n in data %}
-	#     <div>{{n}}</div>
-	#     {% endfor %}
-	#
-	context = dict(data = names)
+        # DEBUG: this is debugging code to see what request looks like
+        print(request.args)
 
 
-	#
-	# render_template looks in the templates/ folder for files.
-	# for example, the below file reads template/index.html
-	#
-	return render_template("index.html", **context)
+        #
+        # example of a database query
+        #
+        select_query = "SELECT g.group_id, t.team_name, g.points, g.ranking from groups as g join teams as t on g.team_id = t.team_id order by g.group_id asc"
+        cursor = g.conn.execute(text(select_query))
+        all_groups = []
+        groupA = []
+        groupB = []
+        groupC = []
+        groupD = []
+        groupE = []
+        groupF = []
+        groupG = []
+        groupH = []
+        for result in cursor:
+            if result[0] == 'A':
+                groupA.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'B':
+                groupB.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'C':
+                groupC.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'D':
+                groupD.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'E':
+                groupE.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'F':
+                groupF.append((result[0],result[1],result[2],result[3]))
+            elif result[0] == 'G':
+                groupG.append((result[0],result[1],result[2],result[3]))
+            else:
+                groupH.append((result[0],result[1],result[2],result[3]))
+        all_groups.append(groupA)
+        all_groups.append(groupB)
+        all_groups.append(groupC)
+        all_groups.append(groupD)
+        all_groups.append(groupE)
+        all_groups.append(groupF)
+        all_groups.append(groupG)
+        all_groups.append(groupH)
+        #print(groupA)
+        #print('---')
+        #print(all_groups)
+        #print(all_groups[0][1][1])
+        cursor.close()
+
+        #
+        # Flask uses Jinja templates, which is an extension to HTML where you can
+        # pass data to a template and dynamically generate HTML based on the data
+        # (you can think of it as simple PHP)
+        # documentation: https://realpython.com/primer-on-jinja-templating/
+        #
+        # You can see an example template in templates/index.html
+        #
+        # context are the variables that are passed to the template.
+        # for example, "data" key in the context variable defined below will be
+        # accessible as a variable in index.html:
+        #
+        #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+        #     <div>{{data}}</div>
+        #
+        #     # creates a <div> tag for each element in data
+        #     # will print:
+        #     #
+        #     #   <div>grace hopper</div>
+        #     #   <div>alan turing</div>
+        #     #   <div>ada lovelace</div>
+        #     #
+        #     {% for n in data %}
+        #     <div>{{n}}</div>
+        #     {% endfor %}
+        #
+        context = dict(data = all_groups)
+
+
+        #
+        # render_template looks in the templates/ folder for files.
+        # for example, the below file reads template/index.html
+        #
+        return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at:
@@ -171,52 +206,52 @@ def index():
 #
 @app.route('/another')
 def another():
-	return render_template("another.html")
+        return render_template("another.html")
 
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
-	# accessing form inputs from user
-	name = request.form['name']
+        # accessing form inputs from user
+        name = request.form['name']
 
-	# passing params in for each variable into query
-	params = {}
-	params["new_name"] = name
-	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-	g.conn.commit()
-	return redirect('/')
+        # passing params in for each variable into query
+        params = {}
+        params["new_name"] = name
+        g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
+        g.conn.commit()
+        return redirect('/')
 
 
 @app.route('/login')
 def login():
-	abort(401)
-	this_is_never_executed()
+        abort(401)
+        this_is_never_executed()
 
 
 if __name__ == "__main__":
-	import click
+        import click
 
-	@click.command()
-	@click.option('--debug', is_flag=True)
-	@click.option('--threaded', is_flag=True)
-	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8111, type=int)
-	def run(debug, threaded, host, port):
-		"""
-		This function handles command line parameters.
-		Run the server using:
+        @click.command()
+        @click.option('--debug', is_flag=True)
+        @click.option('--threaded', is_flag=True)
+        @click.argument('HOST', default='0.0.0.0')
+        @click.argument('PORT', default=8111, type=int)
+        def run(debug, threaded, host, port):
+                """
+                This function handles command line parameters.
+                Run the server using:
 
-			python server.py
+                        python server.py
 
-		Show the help text using:
+                Show the help text using:
 
-			python server.py --help
+                        python server.py --help
 
-		"""
+                """
 
-		HOST, PORT = host, port
-		print("running on %s:%d" % (HOST, PORT))
-		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+                HOST, PORT = host, port
+                print("running on %s:%d" % (HOST, PORT))
+                app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 run()
